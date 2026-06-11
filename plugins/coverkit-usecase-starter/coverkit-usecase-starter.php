8395eb142c794d8c41d6b88c6f043f88dc9a8af5
Xaver <xaver@everpress.co>
2026-06-11 10:13:41 +0200

    Refactor path definition for starter use case inclusion in coverkit-usecase-starter.php to improve clarity and maintainability.

--- plugins/coverkit-usecase-starter/coverkit-usecase-starter.php
+++ plugins/coverkit-usecase-starter/coverkit-usecase-starter.php
@@ -9,12 +9,7 @@ declare(strict_types=1);
 
 defined( 'ABSPATH' ) || exit;
 
-define( 'COVERKIT_USECASE_STARTER_VERSION', '0.1.0' );
-define( 'COVERKIT_USECASE_STARTER_FILE', __FILE__ );
-define( 'COVERKIT_USECASE_STARTER_DIR', plugin_dir_path( __FILE__ ) );
-
-
-require_once COVERKIT_USECASE_STARTER_DIR . 'includes/class-starter-use-case.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-starter-use-case.php';
 
 add_action(
 	'coverkit_init',
